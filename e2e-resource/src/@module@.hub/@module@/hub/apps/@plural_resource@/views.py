56a19d69c9604825508523273fa0fc9ec8d9d82a
BJ Dierkes <wdierkes@rackspace.com>
2012-06-25 15:40:13 -0500

    adding new resources to management tab

--- e2e-resource/src/@module@.hub/@module@/hub/apps/@plural_resource@/views.py
+++ e2e-resource/src/@module@.hub/@module@/hub/apps/@plural_resource@/views.py
@@ -7,56 +7,57 @@ from @module@.hub import db, forms
 
 def list(request):
     data = {}
-    return render(request, 'environments/list.html', data)
+    data['@plural_resource@'] = db.@resource.capitalize@.objects.all()
+    return render(request, '@plural_resource@/list.html', data)
 
-def details(request, env_id):
+def details(request, @resource@_id):
     data = {}
-    data['environment'] = get_object_or_404(db.@resource.capitalize@, id=env_id)
-    return render(request, 'environments/details.html', data)
+    data['@resource@'] = get_object_or_404(db.@resource.capitalize@, id=@resource@_id)
+    return render(request, '@plural_resource@/details.html', data)
 
 def create(request):
     data = {}
     if request.method == 'POST':
         form = forms.@resource.capitalize@Form(request.POST)
         if form.is_valid():
-            env = form.save()
+            @resource@ = form.save()
             messages.add_message(request, messages.SUCCESS, 
-                                 '%s created successfully' % env.label)
-            return redirect(reverse('manage_environments'))
+                                 '%s created successfully' % @resource@.label)
+            return redirect(reverse('manage_@plural_resource@'))
     else:
         form = forms.@resource.capitalize@Form(initial=dict(created_by=request.user))
     
     data['form'] = form
-    return render(request, 'environments/create.html', data)
+    return render(request, '@plural_resource@/create.html', data)
 
-def update(request, env_id):
+def update(request, @resource@_id):
     data = {}
-    env = get_object_or_404(db.@resource.capitalize@, id=env_id)
+    @resource@ = get_object_or_404(db.@resource.capitalize@, id=@resource@_id)
     
     if request.method == 'POST':
-        form = forms.@resource.capitalize@Form(request.POST, instance=env)
+        form = forms.@resource.capitalize@Form(request.POST, instance=@resource@)
         if form.is_valid():
-            env = form.save()
+            @resource@ = form.save()
             messages.add_message(request, messages.SUCCESS, 
-                                 '%s updated successfully' % env.label)
-            return redirect(reverse('update_environment',
-                                    kwargs={'env_id': env.id}))                                
+                                 '%s updated successfully' % @resource@.label)
+            return redirect(reverse('update_@resource@',
+                                    kwargs={'@resource@_id': @resource@.id}))                                
     else:
-        form = forms.@resource.capitalize@Form(instance=env)
+        form = forms.@resource.capitalize@Form(instance=@resource@)
 
     data['form'] = form
-    data['environment'] = env
-    return render(request, 'environments/update.html', data)
+    data['@resource@'] = @resource@
+    return render(request, '@plural_resource@/update.html', data)
     
-def delete(request, env_id):
+def delete(request, @resource@_id):
     data = {}
-    env = get_object_or_404(db.@resource.capitalize@, id=env_id)
-    env.delete()
+    @resource@ = get_object_or_404(db.@resource.capitalize@, id=@resource@_id)
+    @resource@.delete()
     messages.add_message(request, messages.WARNING, 
-                                 '%s has been deleted.' % env.label)
-    return redirect(reverse('manage_environments'))
+                                 '%s has been deleted.' % @resource@.label)
+    return redirect(reverse('manage_@plural_resource@'))
 
 def manage(request):
     data = {}
-    data['environments'] = db.@resource.capitalize@.objects.all()
-    return render(request, 'environments/manage.html', data)
+    data['@plural_resource@'] = db.@resource.capitalize@.objects.all()
+    return render(request, '@plural_resource@/manage.html', data)
